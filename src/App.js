d26ef51f91f7def2af13cf91ff05078f175ff3e4
Abhishek Kanparia <kanpariaabhishek@gmail.com>
2020-09-26 10:50:39 +0530

    Add search
    - Add form field
    - Utilize useBookSearch
    - Use intersectionObserver for infinite scrolling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useState, useRef, useCallback } from 'react'
+import useBookSearch from './useBookSearch'
 
 function App() {
-  return <h1>Infinite Scrolling</h1>
+  const [query, setQuery] = useState('')
+  const [pageNo, setPageNo] = useState(1)
+  const { loading, error, books, hasMore } = useBookSearch(query, pageNo)
+
+  const observer = useRef()
+  const lastBookElementRef = useCallback(
+    (node) => {
+      if (loading) return
+
+      if (observer.current) observer.current.disconnect()
+
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNo((prevPageNo) => prevPageNo + 1)
+        }
+      })
+
+      if (node) observer.current.observe(node)
+    },
+    [loading, hasMore]
+  )
+
+  return (
+    <>
+      <h1>Infinite Scrolling Book search</h1>
+      <input
+        type='text'
+        name='title'
+        onChange={(e) => {
+          setQuery(e.target.value)
+          setPageNo(1)
+        }}
+        value={query}
+      />
+      <ul>
+        {books.map((book, index) => {
+          if (books.length === index + 1)
+            return (
+              <li ref={lastBookElementRef} key={book}>
+                {book}
+              </li>
+            )
+          else return <li key={book}>{book}</li>
+        })}
+      </ul>
+      <div style={{ color: 'red' }}>{loading && 'Loading...'}</div>
+      <div style={{ color: 'red' }}>{error && 'An Error occured'}</div>
+    </>
+  )
 }
 
 export default App
